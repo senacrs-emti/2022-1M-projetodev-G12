03da474a268934f1f8d89e9f6a7d136eb0be0772
Henrique Costa Lima <108465548+HCl1607@users.noreply.github.com>
2022-10-27 08:40:27 -0300

    Add files via upload

---
+++ array/js/script.js
@@ -0,0 +1,40 @@
+// cria a colecao de dados
+const curiosidades  = [
+    
+    "Curiosidade1", 
+    "Curiosidade2", 
+    "Curiosidade3",
+    "Curiosidade4",
+    "Curiosidade5",
+    "Curiosidade6",
+    "Curiosidade7",
+    "Curiosidade8",
+    "Curiosidade9",
+    "Curiosidade10",
+    "Curiosidade11",
+    "Curiosidade12",
+    "Curiosidade13",
+    "Curiosidade14",
+    "Curiosidade15",
+    "Curiosidade16",
+    "Curiosidade17",
+    "Curiosidade18",
+    "Curiosidade19",
+    "Curiosidade20",
+    "Curiosidade21",
+    "Curiosidade22",
+    "Curiosidade23",
+    "Curiosidade24",
+    "Curiosidade25",
+    "Curiosidade26",
+    "Curiosidade27",
+    "Curiosidade28",
+    "Curiosidade29",
+    "Curiosidade30",
+
+];
+Array.isArray(curiosidades);
+
+
+document.getElementById("demo").innerjs =
+Math.floor(Math.random() *30 ) + 1;
