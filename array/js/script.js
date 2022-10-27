fc596d1519a37c730ae0a01938e3017f2ed85346
Henrique Costa Lima <108465548+HCl1607@users.noreply.github.com>
2022-10-27 08:50:04 -0300

    Update script.js

--- array/js/script.js
+++ array/js/script.js
@@ -33,6 +33,53 @@ const curiosidades  = [
     "Curiosidade30",
 
 ];
+
+
+
+
+// Método 2 
+const curiosidade = [30];
+curiosidades[0]= "curiosidades1";
+curiosidades[1]= "curiosidades2";
+curiosidades[3]= "curiosidades3";
+curiosidades[4]= "curiosidades4";
+curiosidades[5]= "curiosidades5";
+curiosidades[6]= "curiosidades6";
+curiosidades[7]= "curiosidades7";
+curiosidades[8]= "curiosidades8";
+curiosidades[9]= "curiosidades9";
+curiosidades[10]= "curiosidades10";
+curiosidades[11]= "curiosidades11";
+curiosidades[12]= "curiosidades12";
+curiosidades[13]= "curiosidades13";
+curiosidades[14]= "curiosidades14";
+curiosidades[15]= "curiosidades15";
+curiosidades[16]= "curiosidades16";
+curiosidades[17]= "curiosidades17";
+curiosidades[18]= "curiosidades18";
+curiosidades[19]= "curiosidades19";
+curiosidades[20]= "curiosidades20";
+curiosidades[21]= "curiosidades21";
+curiosidades[22]= "curiosidades22";
+curiosidades[23]= "curiosidades23";
+curiosidades[24]= "curiosidades24";
+curiosidades[25]= "curiosidades25";
+curiosidades[26]= "curiosidades26";
+curiosidades[27]= "curiosidades27";
+curiosidades[28]= "curiosidades28";
+curiosidades[29]= "curiosidades29";
+curiosidades[31]= "curiosidades30";
+
+
+
+
+
+document.getElementById("demo").innerHTML = curiosidades;
+Array.isArray(curiosidades);
+
+
+document.getElementById("demo").innerjs =
+Math.floor(Math.random() *30 ) + 1;
 Array.isArray(curiosidades);
 
 
